30e1d7a58e3b75baa408d03074012e3423608644
Eva.Dinckel <evadinckel@gmail.com>
2018-03-23 18:11:14 +0000

    update usersratings component and text within

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import HelloWorld from './components/HelloWorld'
+import UsersRatings from './components/UsersRatings'
 // import Index from './public/index.html'
-ReactDOM.render(<HelloWorld />, document.getElementById('app'))
+ReactDOM.render(<UsersRatings />, document.getElementById('app'))
---
+++ components/UsersRatings.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const UsersRatings = () => {
+  return (
+    <div className='users-ratings'>
+      <h1><center>We value your opinion...</center></h1>
+      <p><center>Hello dear Financial Times users! We would like to learn more about your experience with us.</center></p>
+      <p><center>You can give us your rating by using the buttons below</center></p>
+    </div>
+  )
+}
+export default UsersRatings
